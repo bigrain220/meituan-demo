06dc43d07eba2360f389e9f8f6b4862d648446d0
bigrain220 <1573223470@qq.com>
2018-11-30 17:03:11 +0800

    列表样式

--- src/common/SellerList.jsx
+++ src/common/SellerList.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import './SellerList.scss'
-import API from '../api'
-import 'whatwg-fetch'
+// import axios from 'axios';
 
 export default class SellerList extends Component{
-    constructor(){
-        super();
-        this.state = {
-           sellList:[]
-        }
+    constructor(props){
+        super(props);
+        
     }
     render(){
        
@@ -17,37 +14,63 @@ export default class SellerList extends Component{
                 <div className="top-placeholder">
                     <div className="top-placeholder-inner">附近商家</div>
                 </div>
+                <div className="j-main-sec">
+                    <div className="poilist">
+                    {
+                            this.props.sellerArray.map((item, index)=>{
+                                return (
+                                  <div className="field">
+                                    
+                                        <div className="avatar">
+                                            <span className="avatar-brand">品牌</span>
+                                            <img src={item.pic_url} className="j-poi-pic avatar-img"></img>
+                                        </div>
+                                        <div className="content">
+                                            <div className="shop-title shop-title-icon-0">
+                                                <div className="shop-na">{item.name}</div>
+                                            </div>
+                                            <div className="shop-mid-line clearfix">
+                                                <div className="appr-status"></div>
+                                                <div className="shop-sold mtsi-num">月售</div>
+                                                <i className="shop-right mtsi-num">km</i>
+                                                <i className="shop-dvd-line">|</i>
+                                                <i className="shop-delivery-time mtsi-num">min</i>
+                                            </div>
+                                            <div className="shop-line clearfix">
+                                                <span className="shop-line-item">
+                                                     起送价 <em className="mtsi-num">￥</em>
+                                                </span>
+                                                <span class="shop-line-item mtsi-num">
+                                                        <span class="shop-line-item-line">|</span>配送 ¥
+                                                </span>
+                                            </div>
+                                            <div className="item-bottom">
+                                                <ul className="item-bottom-ul">
+                                                        {
+                                                            item.discounts2.map((items,index)=>{
+                                                                return(
+                                                                    <li className="item-bottom-li">
+                                                                        <i><img src={items.icon_url} alt=""/></i>
+                                                                            <p>{items.info}</p>
+                                                                    </li>
+                                                                )
+                                                            })
+                                                         }
+                                                    
+                                                </ul>
+                                            </div>
+                                        </div>
 
+
+                                  </div>    
+                                )
+                            })
+                    }
+                    </div>
+                </div>
             </div>
         )
     }
 
-    componentDidMount(){
-        //请求左边侧边栏的频道数据
-        fetch(API.SELLER_LIST_API,{
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-              },
-              body: JSON.stringify({
-                uuid: 'KG7UUCHyahH99VUgdX3ZMbh42uzJDUg0Jt-IAvaBcv5q-Vpm4upswk-zCr3vjV3_',
-                platform: '3',
-                partner: '4',
-                page_index: '0',
-                apage: '1',
-                mtsi_font_css_version: '20ad699b'
-              })
-        })
-        .then(response => {
-            return response;
-        })
-        .then(result => {
-            // console.log(result)
-            this.setState({sellList: result.data});
-        })
-        .catch(error => {
-            console.log(error);
-        })
-
-    }
+    
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import SellerList from '../../common/SellerList'
 import Top from './Header/Header';
+import 'whatwg-fetch';
+// import API from '../../api';
 import './Home.css';
+import sellerData from './data/sellerList.json'
 
 
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            sellerArray: [],
+        }
+    }
     render(){
+        let {sellerArray} = this.state;
         return (
             <div id="Home"  className="page">
 
@@ -18,11 +28,45 @@ class Home extends Component {
                         </a>
                     </div>
                 </div>
-                <SellerList></SellerList>
+               
+                <SellerList sellerArray={sellerArray}></SellerList>
             </div>
         )
 
     }
+
+    componentDidMount(){
+        
+        // fetch(API.SELLER_LIST_API, {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         uuid: 'KG7UUCHyahH99VUgdX3ZMbh42uzJDUg0Jt-IAvaBcv5q-Vpm4upswk-zCr3vjV3_',
+        //         platform: 3,
+        //         partner: 4,
+        //         page_index: 0,
+        //         apage: 1,
+        //         mtsi_font_css_version: '20ad699b'
+        //     })
+        //   })
+        // .then(response => {
+        //     console.log(response)
+        //     return response
+        //     })
+        // .then(result => {
+        //     console.log(result)
+        //     })
+        // .catch(error => {
+        //         console.log(error)
+        //     })
+          
+            this.setState({sellerArray: sellerData.data.poilist})
+            console.log(sellerData)
+    }
+
+
 }
 
 export default Home;
